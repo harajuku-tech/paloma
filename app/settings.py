2c85340b37f44a84de56687c1a225991151c3deb
hdknr <gmail@hdknr.com>
2012-04-01 09:38:47 +0900

    Postfix extra databases

--- app/settings.py
+++ app/settings.py
@@ -17,6 +17,14 @@ DATABASES = {
         'PASSWORD': 'paloma',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    },
+    'postfix': {
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'postfix',                      # Or path to database file if using sqlite3.
+        'USER': 'postfix',                      # Not used with sqlite3.
+        'PASSWORD': 'postfix',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 

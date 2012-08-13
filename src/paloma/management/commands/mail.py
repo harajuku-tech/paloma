a44ecf06dc5b20ab25f336297b622cd02dfd2226
hdknr <gmail@hdknr.com>
2012-08-14 05:16:35 +0900

    Command: schduling trigger_mail

--- src/paloma/management/commands/mail.py
+++ src/paloma/management/commands/mail.py
@@ -2,6 +2,9 @@
 
 from paloma.management.commands import GenericCommand
 from paloma.mails import send_mail_simple,send_mail_from_file
+from paloma.tasks import trigger_schedule
+
+from django.utils import timezone, dateparse
 
 from optparse import make_option
 from datetime import datetime
@@ -16,6 +19,12 @@ class Command(GenericCommand):
 
     option_list = GenericCommand.option_list + (
 
+        make_option('-t','--time',
+            action='store',
+            dest='time',
+            default=None,
+            help=u'time to trigger'),
+
         )
     ''' Command Option '''
 
@@ -24,6 +33,17 @@ class Command(GenericCommand):
 
         '''
         send_mail_from_file( self.open_file(options) )
+
+    def handle_trigger(self,*args,**options):
+        ''' trigger schedule
+        '''
+        if options['time'] != None:
+            eta = timezone.make_aware( 
+                    dateparse.parse_datetime(options['time']) ,
+                    timezone.get_current_timezone())
+        else :
+            eta=None 
+        trigger_schedule.apply_async(eta=eta)
 
     def handle_help(self,*args,**options):
         '''  help

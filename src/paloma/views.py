b1330ffae0f81e78dcbc11e306cf051c7e878bef
hdknr <gmail@hdknr.com>
2012-08-19 10:49:27 +0900

    Research: making absolute URL

--- src/paloma/views.py
+++ src/paloma/views.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
+import sys
+
+def u(view_name,args=[],kwargs={},absolute=lambda x: x ):
+    return absolute( reverse (view_name, args=args,kwargs=kwargs ) )
 
 def enroll(request,command,secret):
     from django import template
     from django.http import HttpResponse
 
-    print type(request)
+    print sys.argv
     return HttpResponse(
         template.Template("""
         <html><head><title>Paloma Enroll</title></head> <body> <h1> Paloma Enroll</h1>
             {{ url     }} <br/>
+            {{ aurl     }} <br/>
             {{ command }} <br/>
             {{ secret  }} <br/>
         """
         ).render( template.RequestContext(request,
                 {'command':command,'secret':secret,
-                 'url' : request.build_absolute_uri('/o/my/god'),
+#                 'url' : request.build_absolute_uri('/o/my/god'),
+#                 'url' : request.build_absolute_uri('/'),
+                  'url' : u("paloma_enroll",kwargs={"command":"hoge","secret": "xxxx" },),
+                  'aurl' : u("paloma_enroll",kwargs={"command":"hoge","secret": "xxxx" },absolute=request.build_absolute_uri),
                 }) )
     )

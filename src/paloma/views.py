e5750ad0cbba5dfac70ad4803ccb63869e42b19d
hdknr <gmail@hdknr.com>
2012-08-19 09:22:27 +0900

    just improve

--- src/paloma/views.py
+++ src/paloma/views.py
@@ -3,12 +3,17 @@
 def enroll(request,command,secret):
     from django import template
     from django.http import HttpResponse
+
+    print type(request)
     return HttpResponse(
         template.Template("""
         <html><head><title>Paloma Enroll</title></head> <body> <h1> Paloma Enroll</h1>
+            {{ url     }} <br/>
             {{ command }} <br/>
             {{ secret  }} <br/>
         """
         ).render( template.RequestContext(request,
-                {'command':command,'secret':secret}) )
+                {'command':command,'secret':secret,
+                 'url' : request.build_absolute_uri('/o/my/god'),
+                }) )
     )

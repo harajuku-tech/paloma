3640f33966896223997dfb68807e679368c8abd7
hdknr <gmail@hdknr.com>
2012-09-16 10:36:51 +0900

    accounts and food sample (underway)

---
+++ example/app/accounts/views.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response,redirect
+from django import template
+from django.views.generic.edit import FormView
+
+from paloma.models import Group
+from paloma.forms import SignUpMailForm,SignUpWebForm,SignUpWebPreviewForm
+from paloma.actions import EnrollAction
+import sys
+
+
+
+
+class SignUpMailView(FormView):
+    ''' Sign Up via Email
+    '''
+    template_name = "accounts/signup/mail.html" 
+    form_class = SignUpMailForm 
+
+    def get(self, *args, **kwargs):
+        ''' GET '''
+        if self.request.user.is_authenticated():
+            #: TODO: to something ....
+            pass
+
+        return super(SignUpMailView, self).get(*args, **kwargs)
+
+        
+    def post(self, *args, **kwargs):
+        ''' POST '''
+        #: DEFAULT
+        return super(SignUpMailView, self).post(*args, **kwargs)
+
+    def get_initial(self):
+        ''' initial '''
+        return super(SignUpMailView, self).get_initial()
+
+    def get_context_data(self, **kwargs):
+        ''' Context , to the template'''
+        ctx = kwargs
+        ctx.update({
+        })
+        return ctx
+    
+    def form_valid(self, form):
+        ''' When valid.
+        ''' 
+        e = EnrollAction.provide_signup( form.cleaned_data['group'] )
+        email=e.signup_email()
+        #: normatli redirect
+        #: return redirect(self.get_success_url())
+        return self.render_to_response(self.get_context_data(email=email,form=form))
+
+    def form_invalid(self, form):
+        ''' When INvalid
+        '''
+        return super(SignUpMailView,self).form_invalid(form)
+
+class SignUpWebView(FormView):
+    ''' Sign Up via Web 
+    '''
+    template_name = "accounts/signup/web/default.html" 
+    form_class = SignUpWebForm 
+    command = "default"
+
+    def get(self,request, **kwargs):
+        ''' GET '''
+        if self.request.user.is_authenticated():
+            #: TODO: to something ....
+            pass
+
+        return super(SignUpWebView, self).get(request, **kwargs)
+
+        
+    def post(self, request, **kwargs):
+        ''' POST '''
+        #: DEFAULT
+
+        if request.POST.has_key('preview'):
+            self.form_class = SignUpWebPreviewForm
+            self.template_name = "accounts/signup/web/preview.html" 
+            self.command="preview"
+        elif request.POST.has_key('reedit'): 
+            self.form_class = SignUpWebForm
+            self.template_name = "accounts/signup/web/default.html" 
+            self.command="reedit"
+        elif request.POST.has_key('commit'):
+            self.template_name = "accounts/signup/web/commit.html" 
+            self.command="commit"
+            
+        return super(SignUpWebView, self).post(request, **kwargs)
+
+    def get_initial(self):
+        ''' initial '''
+        return super(SignUpWebView, self).get_initial()
+
+    def get_context_data(self, **kwargs):
+        ''' Context , to the template'''
+        ctx = kwargs
+        ctx.update({
+        })
+        return ctx
+    
+    def form_valid(self, form):
+        ''' When valid.
+        ''' 
+        if self.command == "commit":
+            EnrollAction(self.request).enroll_by_web(
+                form.cleaned_data['username']       ,
+                form.cleaned_data['password']       ,
+                form.cleaned_data['email']       ,
+                form.cleaned_data['group']       ,
+            )
+            
+        #:return redirect(self.get_success_url())
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def form_invalid(self, form):
+        ''' When INvalid
+        '''
+        form = self.get_form( SignUpWebForm )   #: Re Edit anyway
+        self.template_name = "paloma/signup/web/default.html" 
+
+        return super(SignUpWebView,self).form_invalid(form)
+
+
+def u(view_name,args=[],kwargs={},absolute=lambda x: x ):
+    return absolute( reverse (view_name, args=args,kwargs=kwargs ) )
+
+
+
+def signin(request):
+    """ Sign In """
+    return render_to_response("accounts/signin.html",{},
+                        context_instance=template.RequestContext(request),)
+
+def signup_form(request):
+    ''' signup via web form
+    '''
+    return render_to_response("accounts/signup/form.html",
+                        {},
+                        context_instance=template.RequestContext(request),)
+
+def enroll(request,command,secret):
+    result = "default"
+    templates = "accounts/enroll/%s/%s.html"
+    user = None 
+    error = None
+
+    if command == "activate":
+        enroll = EnrollAction(request).activate(secret)
+        if enroll == None:
+            result ="error" 
+            error = "invalid url to activate" 
+        user = enroll.mailbox.user
+    else:
+        result,error = "error", "command unknown"
+         
+
+    return render_to_response(
+        templates % ( command,result ),
+        { "user": user , "error" : error },
+        context_instance=template.RequestContext(request),)
+        
+
+def default(request):
+    return render_to_response(
+        "accounts/default.html",
+        { },
+        context_instance=template.RequestContext(request),)
+        

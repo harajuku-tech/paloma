6a0ecacf332adbac55fb357620a06a548df99e6a
hdknr <gmail@hdknr.com>
2012-09-17 12:31:55 +0900

    Now worker management depends the **celery_workman** tool

--- example/app/settings.py
+++ example/app/settings.py
@@ -126,6 +126,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_DIR,'templates'),
 )
 
 INSTALLED_APPS = (
@@ -227,7 +228,10 @@ else:
  
 # -- sample app
 
-INSTALLED_APPS +=('app.foods',)  #:  Sammple application for Paloma
+INSTALLED_APPS +=(
+        'app.accounts', #: Sample Membership Management
+        'app.foods',    #:  Sammple application for Paloma
+        ) 
 
 # -- django-extensinon
 
--- example/app/workers.py
+++ example/app/workers.py
@@ -4,13 +4,13 @@ import sys
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
 APP_DIR=os.path.dirname(__file__)
-LOG_FILE="/tmp/celery.log"  #: celery worker logfile 
-PID_FILE="/tmp/celery.pid"  #: celery worker PID file
-PID_CAM="/tmp/celerycam.pid"
+LOG_FILE="/tmp/paloma.log"  #: celery worker logfile 
+PID_FILE="/tmp/paloma.pid"  #: celery worker PID file
+PID_CAM="/tmp/paloma.pid"
 NODE="celery"           #: celery = default node
 LOG_LEVEL="DEBUG"       #: celery log level
 
-def configure(args):
+def configure(*args):
     ''' return django-celery parameter for specified args
 
         - args[0] : paloma_worker.py
@@ -21,8 +21,7 @@ def configure(args):
     if  len(args) < 3 or args[2] == "start" :
         #: start worker
         #: TODO: Check some exiting process
-        return [ args[0], 
-#                "celeryd",
+        return [ 
                 "celery","worker",
                 "--loglevel=%s" % LOG_LEVEL,
                 "--pidfile=%s"  %  PID_FILE, 
@@ -34,7 +33,7 @@ def configure(args):
 
     if  len(args) >2 and args[2] == "stop":
         #: stop worker
-        return [ args[0],
+        return [
                 "celery","multi",
                 "stop",NODE,
                 "--pidfile=%s" % PID_FILE, 
@@ -42,14 +41,14 @@ def configure(args):
 
     if  len(args) >2 and args[2] == "cam":
         #: TODO: Check some exiting process
-        return [ args[0],
+        return [
                  "celerycam",
                 "--pidfile=%s" % PID_CAM, 
                ]
 
     if  len(args) >2 and args[2] == "camstop":
         #: TODO: Check some exiting process
-        return [ args[0],
+        return [ 
                 "celery","multi",
                 "stop",NODE,
                 "--pidfile=%s" % PID_CAM, 
